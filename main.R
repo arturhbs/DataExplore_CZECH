1a5e6fee07fee387a5e3bdf5a6365aa9cf7996e7
arturhbs <arturhenrique71@gmail.com>
2022-06-18 00:26:24 -0300

    add new function for district, loan and order

--- main.R
+++ main.R
@@ -17,22 +17,13 @@ df_order_raw <- read.csv2(files[7]) %>% as_tibble()
 df_transaction_raw <- read.csv2(files[8]) %>% as_tibble()
 
 df_account_raw 
-  # date = YYMMDD // new  POPLATEK MESICNE -> monthly ; POPLATEK TYDNE weekly ; POPLATEK PO OBRATU -> afterTransaction
 df_credit_card_raw
-  # date = YYMMDD
 df_client_raw 
   # birth number - YYMMDD for men and YYMMDD+50DD for women
-df_disposition_raw # 
+df_disposition_raw
 df_district_raw 
-  # field names = A1 -> district_id; A2 -> district_name; A3->region; A4-> num_inhabitants; A5-> municipal_499; 
-  # A6->  municipal_500K_2K; A7->municipal_2K_10K ; A8 -> municipal_10K; A9 -> num_city; A10 -> ratio_urban_inhabit; 
-  # A11 -> avg_salary; A12 -> unemploy_rate_95; A13 -> unemploy_rate_96 ; A14 -> entrepreneurs_1K_inhabit; 
-  # A15-> num_crimes_95; A16-> num_crimes_96
 df_loan_raw
-  # date = YYMMDD
-  # status = A -> c_finished; B -> c_finished_loan_unpaid; C -> c_running; D -> c_running_debt
 df_order_raw 
-  # K_symbol_meaning POJISTNE -> insurrance_payment ; SIPO -> household_payment ; LEASING -> leasing ; UVER -> loan_payment
 df_transaction_raw
   # date = YYMMDD
   # type = PRIJEM -> credit VYDAJ -> withdrawal
@@ -53,5 +44,7 @@ df_loan <- transformLoan(df_loan_raw)
 df_order <- transformOrder(df_order_raw)
 df_transaction <- transformTransaction(df_transaction_raw)
 
+df_loan
+df_order
+df_district
 
-df_account %>% as_tibble()
--- transform_data.R
+++ transform_data.R
@@ -33,17 +33,41 @@ transformDisposition <- function(dataset){
 }
 
 transformDistrict <- function(dataset){
+  # field names = A1 -> district_id; A2 -> district_name; A3->region; A4-> num_inhabitants; A5-> municipal_499; 
+  # A6->  municipal_500K_2K; A7->municipal_2K_10K ; A8 -> municipal_10K; A9 -> num_city; A10 -> ratio_urban_inhabit; 
+  # A11 -> avg_salary; A12 -> unemploy_rate_95; A13 -> unemploy_rate_96 ; A14 -> entrepreneurs_1K_inhabit; 
+  # A15-> num_crimes_95; A16-> num_crimes_96
+
+  header <- c("district_id", "district_name", "region", "num_inhabitants", "municipal_499", "municipal_500K_2K",
+              "municipal_2K_10K", "municipal_10K", "num_city", "ratio_urban_inhabit", "avg_salary", "unemploy_rate_95", 
+              "unemploy_rate_96", "entrepreneurs_1K_inhabit", "num_crimes_95", "num_crimes_96")
   
+  colnames(dataset) <- header
+
   return(dataset)
 }
 
 transformLoan <- function(dataset){
-  
+  # date = YYMMDD
+  dataset["date"] <- as.Date(as.character(dataset[["date"]]), "%y%m%d")
+  # status = A -> c_finished; B -> c_finished_loan_unpaid; C -> c_running; D -> c_running_debt
+  dataset <- dataset %>% 
+    mutate(status_description = 
+             case_when(status == "A" ~ "c_finished",
+                       status == "B" ~ "c_finished_loan_unpaid", 
+                       status == "C" ~ "c_running",
+                       status == "D" ~ "c_running_debt"))
   return(dataset)
 }
 
 transformOrder <- function(dataset){
-  
+  # K_symbol_meaning POJISTNE -> insurrance_payment ; SIPO -> household_payment ; LEASING -> leasing ; UVER -> loan_payment
+  dataset <- dataset %>% 
+    mutate(k_symbol_description = 
+             case_when(k_symbol == "POJISTNE" ~ "insurrance_payment",
+                       k_symbol == "SIPO" ~ "household_payment", 
+                       k_symbol == "LEASING" ~ "leasing",
+                       k_symbol == "UVER" ~ "loan_payment"))
   return(dataset)
 }
 
